10fa8371e1342be6b435cbbba56fdb74582a87cb
Thomas Mercieca <tmercieca@msn.com>
2023-07-17 01:32:31 +0200

    style: apply formatter

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,7 @@
 
 #include <iostream>
 
-bool hit_sphere(const point3 &center, double radius, const ray &r)
-{
+bool hit_sphere(const point3 &center, double radius, const ray &r) {
   vec3 oc = r.origin() - center;
   auto a = dot(r.direction(), r.direction());
   auto b = 2.0 * dot(oc, r.direction());
@@ -14,7 +13,7 @@ bool hit_sphere(const point3 &center, double radius, const ray &r)
   return (discriminant > 0);
 }
 
-color ray_color(const ray& r) {
+color ray_color(const ray &r) {
   if (hit_sphere(point3(0, 0, -1), 0.5, r))
     return color(1, 0, 0);
   vec3 unit_direction = unit_vector(r.direction());
@@ -22,39 +21,37 @@ color ray_color(const ray& r) {
   return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
-int main()
-{
-    // Image
-    const auto aspect_ratio = 16.0 / 9.0;
-    const int image_Width = 256;
-    const int image_height = static_cast<int>(image_Width / aspect_ratio);
+int main() {
+  // Image
+  const auto aspect_ratio = 16.0 / 9.0;
+  const int image_Width = 256;
+  const int image_height = static_cast<int>(image_Width / aspect_ratio);
 
-    // Camera
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio *  viewport_height;
-    auto focal_length = 1.0;
+  // Camera
+  auto viewport_height = 2.0;
+  auto viewport_width = aspect_ratio * viewport_height;
+  auto focal_length = 1.0;
 
-    auto origin = point3(0,0,0);
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+  auto origin = point3(0, 0, 0);
+  auto horizontal = vec3(viewport_width, 0, 0);
+  auto vertical = vec3(0, viewport_height, 0);
+  auto lower_left_corner =
+      origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
 
-    // Render
-    std::cout << "P3\n" << image_Width << ' ' << image_height << "\n255\n";
+  // Render
+  std::cout << "P3\n" << image_Width << ' ' << image_height << "\n255\n";
 
-    for (int j = image_height - 1; j >= 0; --j)
-    {
-        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+  for (int j = image_height - 1; j >= 0; --j) {
+    std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 
-        for (int i = 0; i< image_Width; ++i)
-        {
-            auto u = double(i) / (image_Width-1);
-            auto v = double(j) / (image_height-1);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-            color pixel_color = ray_color(r);
-            write_color(std::cout, pixel_color);
-        }
+    for (int i = 0; i < image_Width; ++i) {
+      auto u = double(i) / (image_Width - 1);
+      auto v = double(j) / (image_height - 1);
+      ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+      color pixel_color = ray_color(r);
+      write_color(std::cout, pixel_color);
     }
+  }
 
-    std::cerr << "\nDone.\n";
+  std::cerr << "\nDone.\n";
 }
